11574de98ce3424a5a3054088350541f77b6e32c
Karl <unknown@kiln.example.com>
2007-07-13 02:55:11 +0000

    Adding VB test App

--- KRS.ATS.TestApp/Program.cs
+++ KRS.ATS.TestApp/Program.cs
@@ -7,21 +7,32 @@ namespace Krs.Ats.TestApp
 {
     class Program
     {
+        private static int NextOrderId = 0;
+        private static Contract ER2;
+        private static IBClient client;
         static void Main(string[] args)
         {
-            IBClient client = new IBClient();
+            client = new IBClient();
 
             client.TickPrice += client_TickPrice;
             client.TickSize += client_TickSize;
             client.Error += client_Error;
+            client.NextValidId += client_NextValidId;
+
             client.Connect("127.0.0.1", 7496, 10);
-            Contract ER2 = new Contract("ER2", "GLOBEX", SecurityType.Future, "USD", "200709");
+            ER2 = new Contract("ER2", "GLOBEX", SecurityType.Future, "USD", "200709");
             client.ReqMktData(12, ER2, null);
+        }
+
+        static void client_NextValidId(object sender, NextValidIdEventArgs e)
+        {
+            Console.WriteLine("Next Valid Id: " + e.OrderId);
+            NextOrderId = e.OrderId;
         }
 
         static void client_TickSize(object sender, TickSizeEventArgs e)
         {
-            Console.WriteLine("Tick Size: " + e.Size.ToString());
+            Console.WriteLine("Tick Size: " + e.Size);
         }
 
         static void client_Error(object sender, ErrorEventArgs e)
@@ -31,7 +42,7 @@ namespace Krs.Ats.TestApp
 
         static void client_TickPrice(object sender, TickPriceEventArgs e)
         {
-            Console.WriteLine("Price: "+e.Price.ToString());
+            Console.WriteLine("Price: "+e.Price);
         }
     }
 }
---
+++ VBTestApp/Program.vb
@@ -0,0 +1,33 @@
+Imports Krs.Ats.IBNet
+
+Module Program
+    ' Fields
+    Private client As IBClient
+    Private ER2 As Contract
+    Private NextOrderId As Integer
+
+    Sub Main()
+        Program.client = New IBClient
+        AddHandler Program.client.TickPrice, New EventHandler(Of TickPriceEventArgs)(AddressOf Program.client_TickPrice)
+        AddHandler Program.client.TickSize, New EventHandler(Of TickSizeEventArgs)(AddressOf Program.client_TickSize)
+        AddHandler Program.client.Error, New EventHandler(Of ErrorEventArgs)(AddressOf Program.client_Error)
+        AddHandler Program.client.NextValidId, New EventHandler(Of NextValidIdEventArgs)(AddressOf Program.client_NextValidId)
+        Program.client.Connect("127.0.0.1", &H1D48, 10)
+        Program.ER2 = New Contract("ER2", "GLOBEX", SecurityType.Future, "USD", "200709")
+        Program.client.ReqMktData(12, Program.ER2, Nothing)
+
+    End Sub
+    Private Sub client_TickSize(ByVal sender As Object, ByVal e As TickSizeEventArgs)
+        Console.WriteLine(("Tick Size: " & e.Size))
+    End Sub
+    Private Sub client_TickPrice(ByVal sender As Object, ByVal e As TickPriceEventArgs)
+        Console.WriteLine(("Price: " & e.Price))
+    End Sub
+    Private Sub client_NextValidId(ByVal sender As Object, ByVal e As NextValidIdEventArgs)
+        Console.WriteLine(("Next Valid Id: " & e.OrderId))
+        Program.NextOrderId = e.OrderId
+    End Sub
+    Private Sub client_Error(ByVal sender As Object, ByVal e As ErrorEventArgs)
+        Console.WriteLine(("Error: " & e.ErrorMsg))
+    End Sub
+End Module
