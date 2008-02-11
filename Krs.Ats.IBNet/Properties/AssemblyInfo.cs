e583e213d5b1500dccdb4911a8b868ae0fb61f59
Karl <unknown@kiln.example.com>
2008-02-11 03:56:23 +0000

    Fixed Version to 9.4.0.9

--- Krs.Ats.IBNet/Properties/AssemblyInfo.cs
+++ Krs.Ats.IBNet/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly : AssemblyVersion("9.4.0.8")]
-[assembly : AssemblyFileVersion("9.4.0.8")]
+[assembly : AssemblyVersion("9.4.0.9")]
+[assembly : AssemblyFileVersion("9.4.0.9")]
 
 //CLS Compliant
 
