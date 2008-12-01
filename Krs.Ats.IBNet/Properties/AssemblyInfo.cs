6277b65404d8ebcaaf928d54e88556eceff595f3
Karl <unknown@kiln.example.com>
2008-12-01 01:10:13 +0000

    Change Version Number to 9.6.0.11

--- Krs.Ats.IBNet/Properties/AssemblyInfo.cs
+++ Krs.Ats.IBNet/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly : AssemblyVersion("9.5.0.10")]
-[assembly : AssemblyFileVersion("9.5.0.10")]
+[assembly : AssemblyVersion("9.6.0.11")]
+[assembly : AssemblyFileVersion("9.6.0.11")]
 
 //CLS Compliant
 
