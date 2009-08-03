43b2d539864ef9c5960ba44921238ed6d17d6319
Karl <unknown@kiln.example.com>
2009-08-03 19:14:28 +0000

    Increment Version Number.

--- Krs.Ats.IBNet/Properties/AssemblyInfo.cs
+++ Krs.Ats.IBNet/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("Dinosaurtech")]
 [assembly : AssemblyProduct("Krs.Ats.IBNet")]
-[assembly : AssemblyCopyright("Copyright © 2008")]
+[assembly : AssemblyCopyright("Copyright © 2009")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
 
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly : AssemblyVersion("9.6.3.13")]
-[assembly : AssemblyFileVersion("9.6.3.13")]
+[assembly : AssemblyVersion("9.6.3.14")]
+[assembly : AssemblyFileVersion("9.6.3.14")]
 
 //CLS Compliant
 
