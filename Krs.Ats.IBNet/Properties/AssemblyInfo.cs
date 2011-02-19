634b381bd819344fb475f2c22b42ee06c8e26d38
Karl <unknown@kiln.example.com>
2011-02-19 18:37:54 +0000

    MSBuild work.

--- Krs.Ats.IBNet/Properties/AssemblyInfo.cs
+++ Krs.Ats.IBNet/Properties/AssemblyInfo.cs
@@ -9,10 +9,7 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyTitle("Krs.Ats.IBNet")]
 [assembly : AssemblyDescription("Interactive Brokers C# Client")]
 [assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("Dinosaurtech")]
 [assembly : AssemblyProduct("Krs.Ats.IBNet")]
-[assembly : AssemblyCopyright("Copyright © 2011")]
-[assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -24,19 +21,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly : Guid("099cdbe6-c63a-4c1d-9d1e-de9942e56388")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly : AssemblyVersion("9.6.5.18")]
-[assembly : AssemblyFileVersion("9.6.5.18")]
 
 //CLS Compliant
 
