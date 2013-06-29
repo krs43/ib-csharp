f8326154611a581c9615c0f93a82091aa051f43e
Shane Castle <shane.castle@vaultic.com>
2013-06-29 00:31:53 -0400

    Added commission report

---
+++ Krs.Ats.IBNet/CommissionReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/* file created: June, 2013 - Shane Castle - shane.castle@vaultic.com */
+
+namespace Krs.Ats.IBNet
+{
+    public class CommissionReport
+    {
+        private CommissionReport report;
+
+        public String ExecId { get; set; }
+        public double Commission { get; set; }
+        public String Currency { get; set; }
+        public double? RealizedPnL { get; set; }
+        public double? Yield { get; set; }
+        public DateTime? YieldRedemptionDate { get; set; }
+
+        public CommissionReport()
+        {
+        }
+    }
+}
+    
---
+++ Krs.Ats.IBNet/EventArgs/CommissionReportEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/* file created: June, 2013 - Shane Castle - shane.castle@vaultic.com */
+
+namespace Krs.Ats.IBNet
+{
+    /// <summary>
+    /// Commission Report Event Arguments
+    /// </summary>
+    [Serializable()]
+    public class CommissionReportEventArgs : EventArgs
+    {
+        public CommissionReportEventArgs(CommissionReport report)
+        {
+            this.CommissionReport = report;
+        }
+
+        /// <summary>
+        /// Uninitialized Constructor for Serialization
+        /// </summary>
+        public CommissionReportEventArgs() {}
+
+        /// <summary>
+        /// Contains the commission report details.
+        /// </summary>
+        /// <seealso cref="ComissionReport"/>
+        public CommissionReport CommissionReport { get; set; }
+    }
+}
